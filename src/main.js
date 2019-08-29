1474f5987a1c3d140ea528702a913a788801ddc2
Ivan Weber <b24b24testing.ru>
2019-08-29 05:18:36 +0300

    Удалён лишний компонент popup.js, реализована отрисовка соответствующей вёрстки при отсутствии найденных фильмов

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import Search from './components/search.js';
 import Sort from './components/sort.js';
 import ShowMoreButton from './components/show-more-button.js';
 import {Position} from './utils.js';
-import {render} from './utils.js';
-// import {unrender} from './utils.js';
+import {render, unrender} from './utils.js';
 import {getMock} from './data.js';
 import Profile from './components/profile.js';
 import MainNavigation from './components/main-navigation.js';
@@ -11,7 +10,7 @@ import FilmsContainer from './components/films-container.js';
 import FilmCard from './components/film-card.js';
 import FilmDetails from './components/film-details.js';
 import LoadMore from './components/load-more.js';
-import Popup from './components/popup.js';
+import SearchNoResult from './components/search-no-result.js';
 
 
 const NUMBER_OF_FILMS_IN_MAIN_LIST = 12;
@@ -24,6 +23,7 @@ const profileObj = new Profile();
 const mainNavObj = new MainNavigation();
 const filmsContainerObj = new FilmsContainer();
 const filmDetailsObj = new FilmDetails(getMock());
+const searchNoResultObj = new SearchNoResult();
 
 const getFilmsMock = () => {
   let filmsMock = [];
@@ -120,3 +120,15 @@ const cards = cardsWrap.querySelectorAll(`.film-card`);
 const buttonLoadMore = document.querySelector(`.films-list__show-more`);
 const loadMoreButton = new LoadMore(cardsWrap, cards, buttonLoadMore, 5);
 loadMoreButton.initiateLoadMoreButton();
+
+// Если нет фильмов, отрисовываем соответствующую вёрстку
+const siteFooterElement = siteBodyElement.querySelector(`.footer`);
+
+const renderNoResultIfNoFilms = () => {
+  if (siteMainElement.querySelectorAll(`.film-card`).length === 0) {
+    unrender(siteMainElement);
+    render(siteBodyElement, searchNoResultObj.getElement(), siteFooterElement);
+  }
+};
+
+renderNoResultIfNoFilms();
